a9d3652cc942945c517ebe4754557ca1781bc0d3
Loro2060 <102278322+Loro2060@users.noreply.github.com>
2022-12-05 00:23:50 -0500

    Add files via upload

---
+++ XamarinForms/Grid.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        private string mainContentProperty;
+
+        public string MainContentProperty
+        {
+            get { return mainContentProperty; }
+            set { 
+                mainContentProperty = value;
+                OnPropertyChanged(nameof(MainContentProperty));
+
+            }
+        }
+
+        private string exp3;
+        public string Exp3
+        {
+            get { return exp3; }
+            set
+            {
+                exp3 = value;
+                OnPropertyChanged(nameof(Exp3));
+
+            }
+        }
+
+        public Page1(string mainContent)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            MainContentProperty = mainContent;
+        }
+    }
+}
---
+++ XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ButtonSubmitClicked(object sender, EventArgs e)
+        {
+            var input_name = nombre.Text;
+            var input_edad = Edad.Text;
+            var input_dir = Direccion.Text;
+            Application.Current.MainPage.Navigation.PushModalAsync(new Page1(input_name), true);
+        }
+
+        void Button_ClickedOccupationPlus(object sender, EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Ingresa tu ocupacion" };
+            ocupation.Children.Add(entry);
+        }
+    }
+}
